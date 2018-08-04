bb4c440ff984c919adc6b8799f2249c29ba92c38
vfdev <vfdev.5@gmail.com>
2018-08-04 12:56:33 +0200

    Fix for 0.4.1

--- cuda/lltm_cuda.cpp
+++ cuda/lltm_cuda.cpp
@@ -25,8 +25,8 @@ std::vector<at::Tensor> lltm_cuda_backward(
 // C++ interface
 
 // NOTE: AT_ASSERT has become AT_CHECK on master after 0.4.
-#define CHECK_CUDA(x) AT_ASSERT(x.type().is_cuda(), #x " must be a CUDA tensor")
-#define CHECK_CONTIGUOUS(x) AT_ASSERT(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_CUDA(x) AT_ASSERTM(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) AT_ASSERTM(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
 std::vector<at::Tensor> lltm_forward(
