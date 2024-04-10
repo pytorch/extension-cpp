9f5e5b07138171201d629aee6f8be99d7ae2331a
rzou <zou3519@gmail.com>
2024-04-10 14:36:21 -0700

    Set up CI
    [ghstack-poisoned]

---
+++ .github/scripts/unittest.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+./.github/scripts/setup-env.sh
+
+# Activate conda environment
+eval "$($(which conda) shell.bash hook)" && conda deactivate && conda activate ci
+
+echo '::group::Install testing utilities'
+# TODO: remove the <8 constraint on pytest when https://github.com/pytorch/vision/issues/8238 is closed
+pip install --progress-bar=off "pytest<8" pytest-mock pytest-cov expecttest!=0.2.0
+echo '::endgroup::'
