1979f756d9cbbd33b5b64b35e7d7b5b58a6b7c0e
rzou <zou3519@gmail.com>
2024-05-24 06:31:47 -0700

    Replace lltm with myaddmul; update to new custom ops APIs
    We're replacing lltm with myaddmul(a: Tensor, b: Tensor, c: float),
    which just does a*b+c. This simplification allows us to focus on the
    operator registration instead of get lost in the details of the
    complicated lltm kernels.
    
    Test Plan:
    - tests
    
    [ghstack-poisoned]

---
+++ extension_cpp/csrc/muladd.cpp
@@ -0,0 +1,58 @@
+#include <torch/extension.h>
+
+#include <vector>
+
+namespace extension_cpp {
+
+at::Tensor mymuladd_cpu(at::Tensor a, const at::Tensor& b, double c) {
+  TORCH_CHECK(a.sizes() == b.sizes());
+  TORCH_CHECK(a.dtype() == at::kFloat);
+  TORCH_CHECK(b.dtype() == at::kFloat);
+  TORCH_INTERNAL_ASSERT(a.device().type() == at::DeviceType::CPU);
+  TORCH_INTERNAL_ASSERT(b.device().type() == at::DeviceType::CPU);
+  at::Tensor a_contig = a.contiguous();
+  at::Tensor b_contig = b.contiguous();
+  at::Tensor result = torch::empty(a_contig.sizes(), a_contig.options());
+  const float* a_ptr = a_contig.data_ptr<float>();
+  const float* b_ptr = b_contig.data_ptr<float>();
+  float* result_ptr = result.data_ptr<float>();
+  for (int64_t i = 0; i < result.numel(); i++) {
+    result_ptr[i] = a_ptr[i] * b_ptr[i] + c;
+  }
+  return result;
+}
+
+at::Tensor mymul_cpu(const at::Tensor& a, const at::Tensor& b) {
+  TORCH_CHECK(a.sizes() == b.sizes());
+  TORCH_CHECK(a.dtype() == at::kFloat);
+  TORCH_CHECK(b.dtype() == at::kFloat);
+  TORCH_INTERNAL_ASSERT(a.device().type() == at::DeviceType::CPU);
+  TORCH_INTERNAL_ASSERT(b.device().type() == at::DeviceType::CPU);
+  at::Tensor a_contig = a.contiguous();
+  at::Tensor b_contig = b.contiguous();
+  at::Tensor result = torch::empty(a_contig.sizes(), a_contig.options());
+  const float* a_ptr = a_contig.data_ptr<float>();
+  const float* b_ptr = b_contig.data_ptr<float>();
+  float* result_ptr = result.data_ptr<float>();
+  for (int64_t i = 0; i < result.numel(); i++) {
+    result_ptr[i] = a_ptr[i] * b_ptr[i];
+  }
+  return result;
+}
+
+// Registers _C as a Python extension module.
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {}
+
+// Defines the operators
+TORCH_LIBRARY(extension_cpp, m) {
+  m.def("mymuladd(Tensor a, Tensor b, float c) -> Tensor");
+  m.def("mymul(Tensor a, Tensor b) -> Tensor");
+}
+
+// Registers CPU implementations for muladd, mul
+TORCH_LIBRARY_IMPL(extension_cpp, CPU, m) {
+  m.impl("mymuladd", &mymuladd_cpu);
+  m.impl("mymul", &mymul_cpu);
+}
+
+}
--- test/test_extension.py
+++ test/test_extension.py
@@ -9,29 +9,31 @@ import torch.nn.functional as F
 
 
 def sample_inputs(device, *, requires_grad=False):
-    batch_size = 3
-    features = 17
-    state_size = 5
-    kwargs = {"dtype": torch.float64, "device": device, "requires_grad": requires_grad}
-    X = torch.randn(
-        batch_size,  # E: No overload variant of "randn" matches argument
-        features,
-        **kwargs
-    )
-    h = torch.randn(batch_size, state_size, **kwargs)  # E: No overload varia
-    C = torch.randn(batch_size, state_size, **kwargs)  # E: No overload varia
-    W = torch.randn(3 * state_size, features + state_size, **kwargs)
-    b = torch.randn(1, 3 * state_size, **kwargs)  # E: No overload variant of "randn"
-    return X, W, b, h, C
+    def make_tensor(*size):
+        return torch.randn(size, device=device, requires_grad=requires_grad)
+
+    def make_nondiff_tensor(*size):
+        return torch.randn(size, device=device, requires_grad=False)
+
+    return [
+        [make_tensor(3), make_tensor(3), 1],
+        [make_tensor(20), make_tensor(20), 3.14],
+        [make_tensor(20), make_nondiff_tensor(20), -123],
+        [make_nondiff_tensor(2, 3), make_tensor(2, 3), -0.3],
+    ]
 
 
-class TestLLTM(TestCase):
+def reference_muladd(a, b, c):
+    return a * b + c
+
+
+class TestMyMulAdd(TestCase):
     def _test_correctness(self, device):
-        args = sample_inputs(device)
-        result = extension_cpp.ops.lltm(*args)
-        expected = extension_cpp.ops.reference_lltm(*args)
-        self.assertEqual(len(result), len(expected))
-        torch.testing.assert_close(result, expected)
+        samples = sample_inputs(device)
+        for args in samples:
+            result = extension_cpp.ops.mymuladd(*args)
+            expected = reference_muladd(*args)
+            torch.testing.assert_close(result, expected)
 
     def test_correctness_cpu(self):
         self._test_correctness("cpu")
@@ -41,23 +43,31 @@ class TestLLTM(TestCase):
         self._test_correctness("cuda")
 
     def _test_gradients(self, device):
-        args = sample_inputs(device, requires_grad=True)
-        # Use torch.autograd.gradcheck to check that gradients are OK
-        torch.autograd.gradcheck(extension_cpp.ops.lltm, args)
+        samples = sample_inputs(device, requires_grad=True)
+        for args in samples:
+            diff_tensors = [a for a in args if isinstance(a, torch.Tensor) and a.requires_grad]
+            out = extension_cpp.ops.mymuladd(*args)
+            grad_out = torch.randn_like(out)
+            result = torch.autograd.grad(out, diff_tensors, grad_out)
+
+            out = reference_muladd(*args)
+            expected = torch.autograd.grad(out, diff_tensors, grad_out)
+
+            torch.testing.assert_close(result, expected)
 
     def test_gradients_cpu(self):
         self._test_gradients("cpu")
 
-    # This is supposed to succeed, there's probably a bug in the CUDA kernel.
-    @unittest.expectedFailure
     @unittest.skipIf(not torch.cuda.is_available(), "requires cuda")
     def test_gradients_cuda(self):
         self._test_gradients("cuda")
 
     def _opcheck(self, device):
-        args = sample_inputs(device)
-        # Use opcheck to test that the operator was written correctly.
-        opcheck(torch.ops.extension_cpp.lltm_forward.default, args)
+        # Use opcheck to check for incorrect usage of operator registration APIs
+        samples = sample_inputs(device, requires_grad=True)
+        samples.extend(sample_inputs(device, requires_grad=False))
+        for args in samples:
+            opcheck(torch.ops.extension_cpp.mymuladd.default, args)
 
     def test_opcheck_cpu(self):
         self._opcheck("cpu")
